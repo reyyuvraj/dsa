e46d60ee1b55d720f143859708d8b97d5c041b8d
reyyuvraj <yuvrajsingh77733999@gmail.com>
2021-09-09 11:42:25 +0530

    Bit Manipulation

---
+++ src/Bit_FlippedBits.java
@@ -0,0 +1,20 @@
+public class Bit_FlippedBits {
+
+    public static int countBits(int n) {
+        int res = 0;
+        while (n > 0) {
+            n = n & (n - 1);
+            res++;
+        }
+        return res;
+    }
+
+    public static int flippedBits(int a, int b) {
+        return countBits(a ^ b);
+    }
+
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        System.out.println(flippedBits(a, b));
+    }
+}
---
+++ src/Bit_SetBitPosition.java
@@ -0,0 +1,19 @@
+public class Bit_SetBitPosition {
+
+    public static boolean isPowerOfTwo(int n){
+        return ((n>0) && ((n & (n-1))==0));
+    }
+
+    static int findPosition(int n){
+        if(!isPowerOfTwo(n))
+            return -1;
+
+        int i=1,pos=1;
+
+        while ((i & n) ==0){
+            i = i<<1;
+            ++pos;
+        }
+        return pos;
+    }
+}
