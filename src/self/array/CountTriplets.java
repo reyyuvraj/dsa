2461823a9c07ed5de60a5dacdfd9f5c9e3f36075
reyyuvraj <yuvrajsingh77733999@gmail.com>
2021-10-04 20:00:13 +0530

    gfg

--- src/self/array/CountTriplets.java
+++ src/self/array/CountTriplets.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class CountTriplets {
 
     static void triplets(int[] arr, int value) {
-        for (int i = 0; i < arr.length-3; i++) {
+        for (int i = 0; i < arr.length - 3; i++) {
             if (twoSum(arr, value - arr[i], i).length == 2) {
                 System.out.print(arr[i]);
                 System.out.println(Arrays.toString(twoSum(arr, value - arr[i], i)));
@@ -14,7 +14,7 @@ public class CountTriplets {
     }
 
     static int[] twoSum(int[] arr, int sum, int k) {
-        int max = 0, min = arr[k+1], n = arr.length;
+        int max = 0, min = arr[k + 1], n = arr.length;
         for (int i = k; i < n; i++) {
             if (arr[i] + min - sum == 0) {
                 max = arr[i];
@@ -29,8 +29,26 @@ public class CountTriplets {
             return new int[]{0};
     }
 
+    static long lessTriplets(long[] arr,int n, int sum) {
+        Arrays.sort(arr);
+        long res = 0;
+        for(int i=0;i<n-2;i++){
+            int j= i+1, k = n-1;
+            while(j<k){
+                if(arr[i]+arr[j]+arr[k]>=sum)
+                    k--;
+                else{
+                    res += (k-j);
+                    j++;
+                }
+            }
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
-        int[] arr = {5, 1, 3, 4, 7};
-        triplets(arr, 11);
+        long[] arr = {5, 1, 3, 4, 7};
+//        triplets(arr, 11);
+        System.out.print(lessTriplets(arr, arr.length, 12));
     }
 }
