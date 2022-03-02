5bc662804220f662b16c78a117ee7f403f148909
reyyuvraj <yuvrajsingh77733999@gmail.com>
2022-03-02 23:14:00 +0530

    leetcode

--- src/self/array/MergeSortedArray.java
+++ src/self/array/MergeSortedArray.java
@@ -4,23 +4,51 @@ import java.util.Arrays;
 
 public class MergeSortedArray {
 
-    static int[] mergeSortedArray(int[] a, int[] b){
-        int[] merge = new int[a.length+ b.length];
+    static int[] mergeSortedArray(int[] a, int[] b) {
+        int[] merge = new int[a.length + b.length];
 
-        int i=0,j=0,k=0;
+        int i = 0, j = 0, k = 0;
 
-        while (i<a.length && j< b.length){
-            if (a[i]<b[j])
-                merge[k++]=a[i++];
+        while (i < a.length && j < b.length) {
+            if (a[i] < b[j])
+                merge[k++] = a[i++];
             else
-                merge[k++]=b[j++];
+                merge[k++] = b[j++];
         }
         return merge;
     }
 
-    public static void main(String[] args){
-        int[] a = {1,2,3,4,5,6};
-        int[] b = {3,5,7,8};
-        System.out.print(Arrays.toString(mergeSortedArray(a, b)));
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = 0, j = 0, k = 0, num = 0;
+
+        while (i < m || j < n) {
+            if (nums1[i] < nums2[j] && i < m) {
+                num = nums2[j];
+                if (num<nums1[j]){
+                    nums1[k++] = num;
+                    i++;
+                } else {
+                    nums1[k++] = nums1[i++];
+                }
+            } else {
+                num = nums1[i];
+                if (num<nums2[j]){
+                    nums1[k++] = num;
+                    j++;
+                } else {
+                    nums1[k++] = nums2[j++];
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5, 6};
+        int[] b = {3, 5, 7, 8};
+        System.out.println(Arrays.toString(mergeSortedArray(a, b)));
+        int[] c = {1, 2, 3, 0, 0, 0};
+        int[] d = {2, 5, 6};
+        merge(c, 3, d, 3);
+        System.out.println(Arrays.toString(c));
     }
 }
--- src/self/leetcode/NextPermutation.java
+++ src/self/leetcode/NextPermutation.java
@@ -1,25 +1,60 @@
 package self.leetcode;
 
+import java.util.Arrays;
+
 public class NextPermutation {
 
-    public static void reverse(int[] arr, int left, int right) {
+
+    public static void swap(int[] arr, int left, int right) {
         int temp = arr[left];
         arr[left] = arr[right];
         arr[right] = temp;
     }
 
-    public static boolean findNextPermutation(int[] arr) {
+    public static void reverse(int[] arr, int left, int right) {
+
+        while (left < right) {
+            int temp = arr[left];
+            arr[left++] = arr[right];
+            arr[right--] = temp;
+        }
+
+    }
+
+    public static void findNextPermutation(int[] arr) {
 
         if (arr.length <= 1)
-            return false;
+            return;
 
         int last = arr.length - 2;
 
-        return false;
+        while (last >= 0) {
+            if (arr[last] < arr[last + 1]) {
+                break;
+            }
+            last--;
+        }
+
+        if (last >= 0) {
+
+            int nextGreater = arr.length - 1;
+
+            for (int i = arr.length - 1; i > last; i--) {
+                if (arr[i] > arr[last]) {
+                    nextGreater = i;
+                    break;
+                }
+            }
+            swap(arr, nextGreater, last);
+        }
+
+        reverse(arr, last + 1, arr.length - 1);
+
+        System.out.println(Arrays.toString(arr));
     }
 
     public static void main(String[] args) {
-        int[] a = {1, 2, 3};
-
+        int[] a = {3, 2, 1};
+        findNextPermutation(a);
     }
 }
