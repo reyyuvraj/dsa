c8265d444345e4e6ca9fa260f7dea5f533096526
reyyuvraj <yuvrajsingh77733999@gmail.com>
2022-03-15 22:51:07 +0530

    dp

--- src/self/dynamic/NinjaTraining.java
+++ src/self/dynamic/NinjaTraining.java
@@ -2,12 +2,39 @@ package self.dynamic;
 
 import java.util.*;
 
+class Tabulation {
+
+    public int ninjaPoints(int n, int[][] points) {
+
+        int[][] dp = new int[n][4];
+
+        dp[0][0] = Math.max(points[0][1], points[0][2]);
+        dp[0][1] = Math.max(points[0][0], points[0][2]);
+        dp[0][2] = Math.max(points[0][0], points[0][1]);
+        dp[0][3] = Math.max(points[0][0], Math.max(points[0][1], points[0][2]));
+
+        for (int day = 1; day < n; day++) {
+            for (int last = 0; last < 4; last++) {
+                dp[day][last] = 0;
+                for (int task = 0; task < 3; task++) {
+                    if (task != last) {
+                        int activity = points[day][task] + dp[day - 1][task];
+                        dp[day][last] = Math.max(dp[day][last], activity);
+                    }
+                }
+            }
+        }
+
+        return dp[n - 1][3];
+    }
+}
+
 public class NinjaTraining {
 
     public static int ninjaTraining(int n, int[][] points) {
         int[][] dp = new int[n][4];
 
-        for (int[] row: dp)
+        for (int[] row : dp)
             Arrays.fill(row, -1);
 
         return ninjaPoints(n - 1, 3, points, dp);
@@ -46,6 +73,9 @@ public class NinjaTraining {
                 points[i][j] = in.nextInt();
             }
         }
-        System.out.println(ninjaTraining(n, points));
+        System.out.println(ninjaTraining(n, points));// recursion + memoization
+
+        Tabulation ob = new Tabulation();
+        System.out.println(ob.ninjaPoints(n, points));
     }
 }
