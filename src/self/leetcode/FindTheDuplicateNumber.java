35ee0978639e7bed0c896d8e5d9017532762e505
reyyuvraj <yuvrajsingh77733999@gmail.com>
2022-04-26 09:36:24 +0530

    leetcode

--- src/self/leetcode/FindTheDuplicateNumber.java
+++ src/self/leetcode/FindTheDuplicateNumber.java
@@ -2,20 +2,29 @@ package self.leetcode;
 
 public class FindTheDuplicateNumber {
 
-    public static void main(String[] args){
-        int[] arr = {1,2,3,2,4};
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 5, 2, 4};
         System.out.println(duplicateNumber(arr));
     }
 
-    private static int duplicateNumber(int[] arr){
-        int len = arr.length;
+    private static int duplicateNumber(int[] arr) {
+        int n = arr.length;
 
-        return 0;
+        for (int i = 0; i < n; i++) {
+            int num = arr[i];
+            num = Math.abs(num);
+            if (num < n) {
+                if (arr[num] > 0) arr[num] = -arr[num];
+                else return num;
+            }
+        }
+
+        return -1;
     }
 
-    private static int get(int num){
-        int res=0;
-        for (int i=1;i<=num;i++){
+    private static int get(int num) {
+        int res = 0;
+        for (int i = 1; i <= num; i++) {
             res ^= i;
         }
 
