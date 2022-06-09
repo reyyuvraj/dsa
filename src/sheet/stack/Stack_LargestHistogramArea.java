92573f02c48f378b2ecf13365b0659ae71fedc30
reyyuvraj <yuvrajsingh77733999@gmail.com>
2022-06-09 23:13:35 +0530

    leetcode

--- src/sheet/stack/Stack_LargestHistogramArea.java
+++ src/sheet/stack/Stack_LargestHistogramArea.java
@@ -25,11 +25,45 @@ public class Stack_LargestHistogramArea {
         return res;
     }
 
+    public int largestRectangleArea(int[] arr) {
+        Stack<Integer> st = new Stack<>();
+        Integer one;
+        int len = arr.length, max = 0;
+        int[] leftSmaller = new int[len];
+        int[] rightSmaller = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            while (!st.empty() && arr[st.peek()] >= arr[i])
+                st.pop();
+
+            if (st.empty()) leftSmaller[i] = 0;
+            else leftSmaller[i] = st.peek() + 1;
+            st.push(i);
+        }
+
+        while (!st.empty()) st.pop();
+
+        for (int i = len - 1; i >= 0; i--) {
+            while (!st.empty() && arr[st.peek()] >= arr[i])
+                st.pop();
+
+            if (st.empty()) rightSmaller[i] = len - 1;
+            else rightSmaller[i] = st.peek() - 1;
+            st.push(i);
+        }
+
+        for (int i = 0; i < len; i++)
+            max = Math.max((rightSmaller[i] - leftSmaller[i] + 1)*arr[i], max);
+
+
+        return max;
+    }
+
     public static void main(String[] args) {
 
-        int[] arr = new int[]{6, 2, 5, 4, 1, 5, 6};
+        int[] arr = {6, 2, 5, 4, 1, 5, 6};
 
-        System.out.print("Maximum Area: " + getMaxArea(arr, arr.length));
-
+        System.out.println("Maximum Area: " + getMaxArea(arr, arr.length));
+        System.out.println(new Stack_LargestHistogramArea().largestRectangleArea(arr));
     }
 }
